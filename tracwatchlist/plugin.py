83a5f4c31c277b5ab26a7b8dcedd8e7a9010ef67
Hung-Yi Chen <gaodchen@kkbox.com>
2013-08-31 01:03:01 +0800

    Use list to avoid "TypeError: 'set' object does not support indexing"

--- tracwatchlist/plugin.py
+++ tracwatchlist/plugin.py
@@ -527,7 +527,7 @@ class WatchlistPlugin(Component):
                     self.set_notify(req, realm, res)
                 db.commit()
             if redirectback and len(new_res) == 1:
-                req.redirect(req.href(realm,new_res[0]))
+                req.redirect(req.href(realm,list(new_res)[0]))
             action = 'view'
 
         elif action == "unwatch":
